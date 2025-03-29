95e6c727a8fe3c597c1f7a938085f237f7b5e97a
Jonathan Malave <jmaxmalave@aol.com>
2025-03-28 20:31:54 -0400

    Custom Assistant Integration

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,16 @@ require('dotenv').config({path:'../.env'});
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
-//const OpenAI = require("openai");
+const OpenAI = require("openai");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
 
 // Data Base
 
@@ -19,11 +23,55 @@ const db= mysql.createConnection({
 });
 
 app.get('/checkUserExist',(req,res)=> {
-    const sql = "Select * FROM User";
+    const sql = "Select * FROM Courses";
     db.query(sql,(err,data)=>{
         if(err) return res.json(err);
         return res.json(data);
     });
+});
+
+
+// Chat
+app.post("/chat", async (req, res) => {
+    const { query } = req.body;
+
+    if (!query) {
+        return res.status(400).json({ error: "Query is required" });
+    }
+
+    try {
+        console.log(`[${new Date().toTimeString().split(' ')[0]}] Processing: "${query}"`);
+
+        const thread = await openai.beta.threads.create();
+
+        const message = await openai.beta.threads.messages.create(thread.id, {
+            role: "user",
+            content: query,
+        });
+
+        const run = await openai.beta.threads.runs.createAndPoll(thread.id, {
+            assistant_id: process.env.ASSISTANT_API_KEY,
+        });
+
+        if (run.status === "completed") {
+
+            const messages = await openai.beta.threads.messages.list(run.thread_id);
+
+            const responses = messages.data
+                .filter((message) => message.role === "assistant")
+                .map((message) => message.content[0]?.text?.value || "");
+
+            console.log("Finised: OK");
+            return res.json(responses);
+        } else {
+            console.log(`Finised: ${run.status}`);
+            return res.status(200).json({ status: run.status });
+        }
+    } catch (error) {
+        console.error("Error with OpenAI API:", error);
+        return res.status(500).json({ error: "Failed to process the query" });
+    }
+    
 });
 
 app.get("/",(re,res)=> {
